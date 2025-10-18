87846e168134eaad70d8d15740b12637fdbed0b1
Jonas <johannes.hemmett@uvm.edu>
2025-10-17 20:24:35 -0400

    Labels

--- graph.py
+++ graph.py
@@ -2,8 +2,15 @@ import matplotlib.pyplot as plt
 
 sales = [1, 2, 3, 6, 7]
 names = ["J", "O", "N", "A", "S"]
+ids = [101, 102, 103, 104, 105]
 plt.title("Top Customers")
 plt.xlabel('Customer')
 
 plt.bar(names[:3], sales[:3])
+
+# https://www.geeksforgeeks.org/python/matplotlib-pyplot-xticks-in-python/
+
+nr = range(len(names[:3]))
+plt.xticks(nr, [f"{names[i]}\n{ids[i]}" for i in nr])
+
 plt.show()
