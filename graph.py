37a4794446a08700105d71ea0ec5b98a0bf41ae9
Jonas <johannes.hemmett@uvm.edu>
2025-10-18 22:11:42 -0400

    Chart

--- graph.py
+++ graph.py
@@ -1,8 +1,19 @@
 import matplotlib.pyplot as plt
+import json
+with open('data/customers.json', 'r') as file:
+    customers = json.load(file)
 
-sales = [1, 2, 3, 6, 7]
-names = ["J", "O", "N", "A", "S"]
-ids = [101, 102, 103, 104, 105]
+
+# Lambda function sort helper
+getNumTransactions = lambda c: len(c["transactions"])
+customers.sort(key = getNumTransactions, reverse = True)
+
+# https://www.geeksforgeeks.org/python/read-json-file-using-python/
+
+names = [c["firstName"] for c in customers[:3]]
+ids = [c["id"] for c in customers[:3]]
+
+sales = [len(c["transactions"]) for c in customers[:3]]
 plt.title("Top Customers")
 plt.xlabel('Customer')
 
