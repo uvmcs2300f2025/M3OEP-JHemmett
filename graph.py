6f0ffec138f4dc2ed056d16ef3b961a69bac1a43
Jonas <johannes.hemmett@uvm.edu>
2025-10-20 20:00:37 -0400

    Purchases

--- graph.py
+++ graph.py
@@ -5,7 +5,7 @@ def viewCustomerPurchases(size):
     assert size >= 0
 
     # https://www.geeksforgeeks.org/python/read-json-file-using-python/
-    with open('data/customers.json', 'r') as file:
+    with open('data/customersTest.json', 'r') as file:
         customers = json.load(file)
 
     # Lambda function sort helper
@@ -31,11 +31,11 @@ def viewCustomerPurchases(size):
 def customerPurchasesMenu():
     # Finds the number of customers, used 
     try:
-        with open('data/customers.json', 'r') as file:
+        with open('data/customersTest.json', 'r') as file:
             customers = json.load(file)
             maxCustomers = len(customers)
     except:
-        print("Could not open customers.json")
+        print("Could not open customersTest.json")
         maxCustomers = -1
 
     choice = None
@@ -69,26 +69,80 @@ def customerPurchasesMenu():
         print("Invalid Choice")
     viewCustomerPurchases(choice)
 
-def viewVustomer(string id):
-    pass
-plt.show()
+def getCustomer(id):
+    assert id
+
+    # https://www.geeksforgeeks.org/python/read-json-file-using-python/
+    with open('data/customersTest.json', 'r') as file:
+        customers = json.load(file)
+
+    customer = None
+    for c in customers:
+        if c["id"] == id:
+            customer = c
+            break
+    
+    if not customer:
+        print(f"No customer found with id {id}")
+        return
+
+    return customer
+
+def getTransaction(id):
+    assert id
+
+    # https://www.geeksforgeeks.org/python/read-json-file-using-python/
+    with open('data/transactionsTest.json', 'r') as file:
+        transactions = json.load(file)
+
+    transaction = None
+    for t in transactions:
+        if t["id"] == id:
+            transaction = t
+            break
+    
+    if not transaction:
+        print(f"No transaction found with id {id}")
+        return
+
+    return transaction
+
+def viewCustomer(id):
+    customer = getCustomer(id)
+    
+    if not customer:
+        return
+
+    transactions = customer["transactions"]
+    plt.xlabel(f"Purchases")
+
+    plt.bar(customer["transactions"], [getTransaction(t)["totalCost"] for t in customer["transactions"]])
+    
+    plt.subplots_adjust(top=0.75) 
+    plt.figtext(0.1, 0.8, f"Name: {customer['firstName']} {customer['lastName']}\nID: {customer['id']}\nStore Credit: {customer['credit']}", ha = "left", va = "bottom", fontsize = 15)
+
+    plt.show()
+
 if __name__ == "__main__":
 
     choice = None
     while True:
         try:
-            choice = int(input("What graphs would you like to view?\n(1: Customer Purchases): "))
+            choice = int(input("What graphs would you like to view?\n(1: Customer Purchases, 2: Customer info): "))
 
 
         except:
             None
         
-        if not(choice < 0 or choice > 1):
+        if not(choice < 0 or choice > 2):
             break
         
         print("Invalid Choice")
+    
     match choice:
         case 1:
             customerPurchasesMenu()
+        case 2:
+            viewCustomer(1)
     
     
