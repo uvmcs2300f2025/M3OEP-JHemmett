f069b083a4107eea2aa4db8c9f6c720cf0f71c89
Jonas <johannes.hemmett@uvm.edu>
2025-10-21 20:18:42 -0400

    Work

--- graph.py
+++ graph.py
@@ -1,5 +1,62 @@
 import matplotlib.pyplot as plt
 import json
+def getCustomer(id):
+    assert id
+
+    # https://www.geeksforgeeks.org/python/read-json-file-using-python/
+    with open('data/customersTest.json', 'r') as file:
+        customers = json.load(file)
+
+    customer = None
+    for c in customers:
+        if str(c["id"]) == str(id):
+            customer = c
+            break
+    
+    if not customer:
+        print(f"No customer found with id {id}")
+        return
+
+    return customer
+
+def getTransaction(id):
+    assert id
+
+    # https://www.geeksforgeeks.org/python/read-json-file-using-python/
+    with open('data/transactionsTest.json', 'r') as file:
+        transactions = json.load(file)
+
+    transaction = None
+    for t in transactions:
+        if str(t["id"]) == str(id):
+            transaction = t
+            break
+    
+    if not transaction:
+        print(f"No transaction found with id {id}")
+        return
+
+    return transaction
+
+def getItem(id):
+    assert id
+
+    # https://www.geeksforgeeks.org/python/read-json-file-using-python/
+    with open('data/itemsTest.json', 'r') as file:
+        items = json.load(file)
+
+    item = None
+    for i in items:
+        if str(i["id"]) == str(id):
+            item = i
+            break
+    
+    if not item:
+        print(f"No item found with id {id}")
+        return
+
+    return item
+
 
 def viewCustomerPurchases(size):
     assert size >= 0
@@ -69,44 +126,6 @@ def customerPurchasesMenu():
         print("Invalid Choice")
     viewCustomerPurchases(choice)
 
-def getCustomer(id):
-    assert id
-
-    # https://www.geeksforgeeks.org/python/read-json-file-using-python/
-    with open('data/customersTest.json', 'r') as file:
-        customers = json.load(file)
-
-    customer = None
-    for c in customers:
-        if c["id"] == id:
-            customer = c
-            break
-    
-    if not customer:
-        print(f"No customer found with id {id}")
-        return
-
-    return customer
-
-def getTransaction(id):
-    assert id
-
-    # https://www.geeksforgeeks.org/python/read-json-file-using-python/
-    with open('data/transactionsTest.json', 'r') as file:
-        transactions = json.load(file)
-
-    transaction = None
-    for t in transactions:
-        if t["id"] == id:
-            transaction = t
-            break
-    
-    if not transaction:
-        print(f"No transaction found with id {id}")
-        return
-
-    return transaction
-
 def viewCustomer(id):
     customer = getCustomer(id)
     
@@ -117,9 +136,18 @@ def viewCustomer(id):
     plt.xlabel(f"Purchases")
 
     plt.bar(customer["transactions"], [getTransaction(t)["totalCost"] for t in customer["transactions"]])
+
+    tr = range(len(customer["transactions"]))
+
+    plt.subplot
+
+
     
     plt.subplots_adjust(top=0.75) 
+
     plt.figtext(0.1, 0.8, f"Name: {customer['firstName']} {customer['lastName']}\nID: {customer['id']}\nStore Credit: {customer['credit']}", ha = "left", va = "bottom", fontsize = 15)
+    
+    # plt.xticks(tr, [(getItem(iId)["name"] if getItem(iId) else "(Out of Stock)") + "\n" + iId for iId in customer["transactions"]])
 
     plt.show()
 
